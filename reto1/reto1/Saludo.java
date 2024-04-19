7ed1f907035d995ca7644d75bd8525948d630754
BryanArredon <164120862+BryanArredon@users.noreply.github.com>
2024-04-18 23:08:50 -0600

    Add files via upload

---
+++ reto1/reto1/Saludo.java
@@ -0,0 +1,12 @@
+package mx.utng.reto1;
+
+public class Saludo {
+    
+    public static void main(String[] args){
+        System.out.println("¡Hola Bryan!");
+    }
+}
+
+
+////////Bryan Emilio Arredondo Lopez
+////////GDS0624
---
+++ s31/src/main/java/mx/utng/s31/S31Application.java
@@ -0,0 +1,17 @@
+package mx.utng.s31;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S31Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S31Application.class, args);
+	}
+
+}
+
+
+///////////////Bryan Emilio Aredondo Lopez
+////////GDS0624
